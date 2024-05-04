326d68fd48c26a7d73fd7481aa65ce79b0cdb550
AlexKur161 <78803205+AlexKur161@users.noreply.github.com>
2024-05-04 17:11:23 +0300

    2.1 - 2.10 chapter

---
+++ 2 глава/chapter_2_10.js
@@ -0,0 +1,24 @@
+let value = prompt('Какое "официальное" название JavaScript?', '');
+
+    if (value == 'ECMAScript') {
+      alert('Верно!');
+    } else {
+      alert('Не знаете? ECMAScript!');
+    }
+
+let value2 = prompt('Введите число', 0);
+
+if (value2 > 0) {
+  alert( 1 );
+} else if (value2 < 0) {
+  alert( -1 );
+} else {
+  alert( 0 );
+}
+
+let result = (a + b < 4) ? 'Мало' : 'Много';
+
+
+let message = (login == 'Сотрудник') ? 'Привет' :
+(login == 'Директор') ? 'Здравствуйте' : 
+(login == '') ? 'Нет логина' : '';
