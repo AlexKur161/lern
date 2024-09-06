3af2ac88002ad4d08dc9ccd3895e092198d093ed
AlexKur <AlexKur-web@yandex.ru>
2024-09-07 01:30:53 +0300

    глава 6.9

---
+++ 6 глава/6.9.js
@@ -0,0 +1,47 @@
+/* Создайте декоратор spy(func), который должен возвращать обёртку, которая сохраняет все вызовы функции в своём свойстве calls.
+
+Каждый вызов должен сохраняться как массив аргументов.
+
+Например: */
+function work(a, b) {
+  alert( a + b ); // произвольная функция или метод
+}
+function spy(func) {
+ 	function test (...arg){
+  	test.calls.push(arg);
+  	return func.apply(this, arg);
+  }
+  test.calls = [];
+  return test;
+}
+work = spy(work);
+
+work(1, 2); // 3
+work(4, 5); // 9
+ for (let args of work.calls) {
+  alert(`call: ${args}`); // "call:1,2", "call:4,5"
+}
+
+/* Задерживающий декоратор
+важность: 5
+Создайте декоратор delay(f, ms), который задерживает каждый вызов f на ms миллисекунд. Например:
+ */
+function f(x) {
+  alert(x);
+}
+function delay(func, time) {
+	function later(...arg){
+   setTimeout(() => func.apply(this, arg), time);
+  }
+  return later;
+}
+
+// создаём обёртки
+let f1000 = delay(f, 10000);
+let f1500 = delay(f, 15000);
+
+ f1000("test"); // показывает "test" после 1000 мс 
+f1500("test2"); // показывает "test" после 1500 мс
+/* Другими словами, delay(f, ms) возвращает вариант f с «задержкой на ms мс».
+
+В приведённом выше коде f – функция с одним аргументом, но ваше решение должно передавать все аргументы и контекст this. */
